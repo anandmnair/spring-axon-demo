0ea893ce4c27664ea8b71c0d6dc49b37c7200140
anandmnair <anand.manissery@gmail.com>
2017-09-11 03:19:55 +0530

    Test case fixed

--- src/main/java/con/anand/axon/demo/account/transfer/MoneyTransferSaga.java
+++ src/main/java/con/anand/axon/demo/account/transfer/MoneyTransferSaga.java
@@ -1,11 +1,8 @@
 package con.anand.axon.demo.account.transfer;
 
-import java.util.UUID;
-
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventhandling.saga.EndSaga;
 import org.axonframework.eventhandling.saga.SagaEventHandler;
-import org.axonframework.eventhandling.saga.SagaLifecycle;
 import org.axonframework.eventhandling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,30 +22,23 @@ public class MoneyTransferSaga {
 	private transient CommandGateway commandGateway;
 	
 	private String targetAccount;
-
-	private String transactionId;
 	
-	private String transferId;
-
 	
 	@StartSaga
 	@SagaEventHandler(associationProperty="transferId")
 	public void on(MoneyTransferRequestedEvent event) {
 		this.targetAccount=event.getTargetAccount();
-		this.transferId=event.getTransferId();
-		transactionId=UUID.randomUUID().toString();
-		SagaLifecycle.associateWith("transactionId",transactionId);
-		commandGateway.send(new WithdrawMoneyCommand(event.getSourceAccount(), transactionId, event.getAmount()));
+		commandGateway.send(new WithdrawMoneyCommand(event.getSourceAccount(), event.getTransferId(), event.getAmount()));
 	}
 
-	@SagaEventHandler(associationProperty="transactionId")
+	@SagaEventHandler(associationProperty="transactionId", keyName="transferId")
 	public void on(MoneyWithdrawnEvent event) {
 		commandGateway.send(new DepositMoneyCommand(this.targetAccount, event.getTransactionId(), event.getAmount()));
 	}
 	
-	@SagaEventHandler(associationProperty="transactionId")
+	@SagaEventHandler(associationProperty="transactionId", keyName="transferId")
 	public void on(MoneyDepositEvent event) {
-		commandGateway.send(new CompleteMoneyTransferCommand(transferId));
+		commandGateway.send(new CompleteMoneyTransferCommand(event.getTransactionId()));
 	}
 	
 	@EndSaga
